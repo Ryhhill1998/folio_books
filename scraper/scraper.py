ec94434479bcba66ff9117e77f08e03f8dbfc820
ryan <ryan.henzell-hill@qa-training.net>
2023-10-17 16:27:58 +0100

    json file added after book data scraped

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import uuid, json
 
 BASE_URL = "https://www.waterstones.com/books/bestsellers"
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60"}
@@ -9,12 +10,14 @@ soup = BeautifulSoup(html, "html.parser")
 
 book_previews = soup.find_all("div", class_="book-preview")
 
+json_data = {"data": {"books": []}}
+
 for preview in book_previews:
     title = preview.find("a", class_="title").text
     img_src = preview.find("img")["data-src"]
     author = preview.find("a", class_="text-gold").text
     price_container = preview.find("div", class_="book-price")
-    price = preview.find_all("span", class_="price")[-1].text.strip()
+    price = float(preview.find_all("span", class_="price")[-1].text.strip().split("\u00a3")[-1])
     star_rating_container = preview.find("div", class_="star-rating")
     stars = 0
 
@@ -31,5 +34,14 @@ for preview in book_previews:
     else:
         stars = None
 
-    
-    
+    json_data["data"]["books"].append({
+        "id": str(uuid.uuid4()),
+        "title": title,
+        "author": author,
+        "image_src": img_src,
+        "price": price,
+        "stars": stars
+    })
+
+with open("../../books.json", "w+") as file:
+    file.write(json.dumps(json_data))
