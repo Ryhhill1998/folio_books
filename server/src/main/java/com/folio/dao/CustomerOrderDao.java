776a040a6e55cdf7882877d1d6a1b7af63ad782d
victoriadw <victoria.daramywilliams@gmail.com>
2023-10-27 10:14:40 +0100

    DAOs updated

--- server/src/main/java/com/folio/dao/CustomerOrderDao.java
+++ server/src/main/java/com/folio/dao/CustomerOrderDao.java
@@ -1,114 +1,77 @@
 package com.folio.dao;
 
 import com.folio.model.CustomerOrder;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class CustomerOrderDao {
 
-    private Connection connection;
+    private JdbcTemplate jdbcTemplate;
 
-    public CustomerOrderDao(Connection connection) {
-        this.connection = connection;
+    public CustomerOrderDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void createCustomerOrder(CustomerOrder customerOrder) throws SQLException {
-        // Fetch the max customer ID and increment by 1 to create the new ID
+    public void createCustomerOrder(CustomerOrder customerOrder) {
+        // Fetch the max customer order ID and increment by 1 to create the new ID
         int lastCustomerOrderId = getLastCustomerOrderId();
         int newCustomerOrderId = lastCustomerOrderId + 1;
 
         // Prepare the SQL INSERT statement
-        String sql = "INSERT INTO customer_order (id, customer_id, date_, status) " +
+        String sql = "INSERT INTO customer_order (id, customer_id, status) " +
                 "VALUES (?, ?, ?, ?)";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, newCustomerOrderId);
-            statement.setInt(2, customerOrder.getCustomer_id());
-            statement.setDate(3, java.sql.Date.valueOf(customerOrder.getDate_()));
-            statement.setString(4, customerOrder.getStatus_());
-            statement.executeUpdate();
-        }
+        jdbcTemplate.update(sql, newCustomerOrderId, customerOrder.getCustomer_id(), customerOrder.getStatus_());
     }
 
-    private int getLastCustomerOrderId() throws SQLException {
+    private int getLastCustomerOrderId() {
         String sql = "SELECT MAX(id) FROM customer_order";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet resultSet = statement.executeQuery()) {
-            if (resultSet.next()) {
-                return resultSet.getInt(1);
-            }
-            // If the table is empty, return 0 or any other default value as needed
+        Integer maxId = jdbcTemplate.queryForObject(sql, Integer.class);
+        if (maxId == null) {
             return 0;
+        } else {
+            return maxId;
         }
     }
 
-
-    public CustomerOrder getCustomerOrderById(int id) throws SQLException {
+    public CustomerOrder getCustomerOrderById(int id) {
         String sql = "SELECT * FROM customer_order WHERE id = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, id);
-
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    return mapResultSetToCustomerOrder(resultSet);
-                }
-                return null; // CustomerOrder not found
-            }
-        }
+        return jdbcTemplate.queryForObject(sql, new CustomerOrderMapper(), id);
     }
 
-    public List<CustomerOrder> getCustomerOrdersByCustomerId(int customerId) throws SQLException {
-        List<CustomerOrder> customerOrders = new ArrayList<>();
+    public List<CustomerOrder> getCustomerOrdersByCustomerId(int customerId) {
         String sql = "SELECT * FROM customer_order WHERE customer_id = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, customerId);
-
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    customerOrders.add(mapResultSetToCustomerOrder(resultSet));
-                }
-            }
-        }
-
-        return customerOrders;
+        return jdbcTemplate.query(sql, new CustomerOrderMapper(), customerId);
     }
 
-    public void updateCustomerOrder(CustomerOrder customerOrder) throws SQLException {
-        String sql = "UPDATE customer_order SET customer_id = ?, date_ = ?, status = ? WHERE id = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, customerOrder.getCustomer_id());
-            statement.setDate(2, java.sql.Date.valueOf(customerOrder.getDate_()));
-            statement.setString(3, customerOrder.getStatus_());
-            statement.setInt(4, customerOrder.getId());
-            statement.executeUpdate();
-        }
+    public CustomerOrder getBasketCustomerOrdersByCustomerId(int customerId) {
+        String sql = "SELECT * FROM customer_order WHERE customer_id = ? AND status_ = ?";
+        return jdbcTemplate.queryForObject(sql, new CustomerOrderMapper(), customerId, "Basket");
     }
 
-    public void deleteCustomerOrder(int id) throws SQLException {
+    public void updateCustomerOrder(CustomerOrder customerOrder) {
+        String sql = "UPDATE customer_order SET customer_id = ?, status = ? WHERE id = ?";
+        jdbcTemplate.update(sql, customerOrder.getCustomer_id(), customerOrder.getStatus_(), customerOrder.getId());
+    }
+
+    public void deleteCustomerOrder(int id) {
         String sql = "DELETE FROM customer_order WHERE id = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, id);
-            statement.executeUpdate();
-        }
+        jdbcTemplate.update(sql, id);
     }
 
-    private CustomerOrder mapResultSetToCustomerOrder(ResultSet resultSet) throws SQLException {
-        CustomerOrder customerOrder = new CustomerOrder();
-        customerOrder.setId(resultSet.getInt("id"));
-        customerOrder.setCustomer_id(resultSet.getInt("customer_id"));
-        customerOrder.setDate_(resultSet.getDate("date_").toLocalDate());
-        customerOrder.setStatus_(resultSet.getString("status"));
-        return customerOrder;
+    private static class CustomerOrderMapper implements RowMapper<CustomerOrder> {
+        @Override
+        public CustomerOrder mapRow(ResultSet resultSet, int i) throws SQLException {
+            CustomerOrder customerOrder = new CustomerOrder();
+            customerOrder.setId(resultSet.getInt("id"));
+            customerOrder.setCustomer_id(resultSet.getInt("customer_id"));
+            customerOrder.setStatus_(resultSet.getString("status"));
+            return customerOrder;
+        }
     }
 }
