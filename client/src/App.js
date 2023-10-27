c6b84b26dc4d94ef07bb4f97ca5906c24d62d175
Ryan <ryhhill1998@outlook.com>
2023-10-27 10:47:51 +0100

    updated import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import Nav from "./components/Nav/Nav";
 import { useProfileStore } from "./globalStores/profile";
@@ -6,6 +6,8 @@ import Auth from "./components/Auth/Auth";
 import HomePage from "./components/HomePage/HomePage";
 
 const App = () => {
+  const [showCheckout, setShowCheckout] = useState(false);
+
   const signedIn = useProfileStore((store) => store.signedIn);
   const clearErrors = useProfileStore((store) => store.clearErrors);
 
--- server/src/main/java/com/folio/FolioBooksApplication.java
+++ server/src/main/java/com/folio/FolioBooksApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.folio;
+package com.folio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
